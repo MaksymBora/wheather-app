684684d871fcd62b0750c337e1a16d15a39722ab
Benedikt <maxboraod@gmail.com>
2024-01-11 16:20:03 +0100

    add fetch data

---
+++ src/components/Navigation.vue
@@ -0,0 +1,64 @@
+<template>
+  <header>
+    <nav class="container">
+      <RouterLink :to="{ name: 'home' }">
+        <div class="items">
+          <i class="fa-solid fa-sun" style="font-size: 24px"></i>
+          <p class="title">The Weather Forecast</p>
+        </div>
+      </RouterLink>
+
+      <div class="info-icon-wrapper">
+        <!-- <i class="fa-solid fa-star" style="font-size: 24px"></i> -->
+        <i class="fa-solid fa-plus" style="font-size: 24px"></i>
+      </div>
+    </nav>
+  </header>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+
+<style scoped>
+header {
+  position: sticky;
+  top: 0;
+  background-color: #00668a;
+}
+.container {
+  padding: 2rem;
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  color: white;
+  padding-top: 24px;
+  padding-bottom: 24px;
+  box-shadow: 1px 6px 10px 0px rgba(0, 0, 0, 0.75);
+}
+
+.items {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  color: white;
+}
+
+.title {
+  font-size: 24px;
+  color: white;
+}
+
+.info-icon-wrapper {
+  display: flex;
+  gap: 12px;
+  flex: 1;
+  justify-content: end;
+}
+
+.fa-solid:hover {
+  color: #004e71;
+  transition: all 250ms ease-in-out;
+  cursor: pointer;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,134 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+import axios from "axios";
+
+const API_KEY = "90909333051e5594b4d9ee0fca1d9913";
+const searchQuery = ref("");
+const queryTimeout = ref(null);
+
+const searchResultCity = ref(null);
+
+const searchResults = ref(null);
+
+const searchError = ref(null);
+
+const getSearchResults = () => {
+  clearTimeout(queryTimeout.value);
+
+  queryTimeout.value = setTimeout(async () => {
+    if (searchQuery.value !== "") {
+      try {
+        const result = await axios.get(
+          `https://api.openweathermap.org/data/2.5/forecast?q=${searchQuery.value}&units=metric&appid=${API_KEY}`
+        );
+        searchResults.value = result.data.list;
+
+        searchResultCity.value = result.data.city;
+      } catch {
+        searchError.value = true;
+      }
+      return;
+    }
+    searchResults.value = null;
+    searchResultCity.value = null;
+    data.value = null;
+  }, 300);
+};
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
+  <main class="container">
+    <div class="wrapper">
+      <input
+        v-model="searchQuery"
+        @input="getSearchResults"
+        type="text"
+        class="city-input"
+        placeholder="Search for a city"
+      />
+
+      <p class="result-info" v-if="searchError">Sorry, something went wrong!</p>
+      <p v-if="!searchError && searchResults?.length === 0">
+        No results match your query, try a different term.
+      </p>
+      <table v-if="searchResults" class="weatherTable">
+        <thead>
+          <tr>
+            <th>Date and Time</th>
+            <th>Min Temperature</th>
+            <th>Max Temperature</th>
+            <th>Weather Condition</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="searchResult in searchResults"
+            :key="searchResult.dt"
+            class="weatherRow"
+          >
+            <td>{{ searchResult.dt_txt }}</td>
+            <td>{{ Math.floor(searchResult.main.temp_min) }}</td>
+            <td>{{ Math.floor(searchResult.main.temp_max) }}</td>
+            <td>{{ searchResult.weather[0].main }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </main>
 </template>
+
+<style scoped>
+.container {
+  padding: 2rem;
+  color: white;
+  padding-top: 24px;
+  padding-bottom: 24px;
+}
+
+.wrapper {
+  position: relative;
+  padding: 16px 0;
+}
+
+.city-input {
+  padding: 8px 4px;
+  width: 100%;
+  background: transparent;
+  border: none;
+  border-bottom: 1px solid white;
+  color: white;
+  text-align: center;
+}
+
+.city-input::placeholder {
+  color: rgb(148, 146, 143);
+  text-align: center;
+}
+
+.city-input:focus {
+  border-color: #004e71;
+  outline: none;
+  box-shadow: 0px 1px 0px 0px #004e71;
+}
+
+.weatherTable {
+  width: 100%;
+  border-collapse: collapse;
+  margin-top: 16px;
+}
+
+.weatherTable th,
+.weatherTable td {
+  border: 1px solid white;
+  padding: 8px;
+  text-align: center;
+}
+
+.weatherRow:hover {
+  background-color: #003c5a;
+}
+
+.result-info {
+  padding: 8px 0;
+}
+</style>
